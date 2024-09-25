661cd98b36ba9b9bfa2322bccb5f8ddee6489392
Jeff Orshalick <jorshalick@gmail.com>
2024-09-24 19:51:31 -0700

    shortens mailto handler visibility

--- static/js/mailto-handler.js
+++ static/js/mailto-handler.js
@@ -7,31 +7,26 @@ document.addEventListener('DOMContentLoaded', function() {
             event.preventDefault(); // Prevents the default mailto behavior
             const emailAddress = link.getAttribute('href').replace('mailto:', '');
 
-            // Copy the email address to the clipboard
             navigator.clipboard.writeText(emailAddress).then(function() {
-                // Show the notification bar
                 showNotification();
 
-                // Hide the notification bar after 3 seconds
-                setTimeout(hideNotification, 3000);
+                setTimeout(hideNotification, 2000);
             }).catch(function(error) {
                 console.error('Could not copy email address: ', error);
             });
         });
     });
 
-    // Function to show the notification
     function showNotification() {
         notificationBar.classList.remove('hidden', 'opacity-0');
         notificationBar.classList.add('opacity-100');
     }
 
-    // Function to hide the notification
     function hideNotification() {
         notificationBar.classList.remove('opacity-100');
         notificationBar.classList.add('opacity-0');
         setTimeout(function() {
             notificationBar.classList.add('hidden');
-        }, 300); // Extra delay to allow opacity transition to complete
+        }, 300); 
     }
 });
